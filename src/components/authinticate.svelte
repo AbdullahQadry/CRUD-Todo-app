969d5459f3c461f3711b079f337135b94d2b8359
Abdulla.qadry <abdullah.qadry@hypcloud.de>
2024-11-05 23:18:46 +0100

    Login layout and other structural preparations

---
+++ src/components/authinticate.svelte
@@ -0,0 +1,53 @@
+<div class="AuthContainer">
+    <form action="">
+
+        <h1> Login </h1>
+
+    <label for="email"></label>
+    <input type="email" id="email" name="email" placeholder="Email" />
+    
+    <label for="email"></label>
+    <input type="email" id="email" name="email" placeholder="Password" />
+    
+    <label for="email"></label>
+    <input type="email" id="email" name="email" placeholder="Repeat password" />
+
+
+    
+    </form>
+
+
+
+</div>
+
+<style>
+
+.AuthContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #f0f0f0;
+}
+
+
+form {
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    align-items: center;
+    text-align: left;
+    gap: 20px;
+    width: 50%;
+    height: 50%;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(56, 21, 255, 0.1);
+    padding: 20px;
+}
+
+
+
+</style>
