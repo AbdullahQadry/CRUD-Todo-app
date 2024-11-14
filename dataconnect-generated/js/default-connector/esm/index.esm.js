b92b59d882d852b4876aa858a70a4ae7d111e193
Abdulla.qadry <abdullah.qadry@hypcloud.de>
2024-11-14 16:52:48 +0100

    Revert "deploying"
    This reverts commit 30343c13a16f130961b225fd300ba9e4afd52aa6.

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'CODE-TODO',
-  location: 'us-central1'
-};
-
